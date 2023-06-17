e70f94327b3320fa2ce2fdf5943aa6bbc4d19bc7
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-16 18:15:22 -0700

    Update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 97; i++)
+	{	putchar(i);
+	}
+	printf("\n");
+
+	return (0);
+}
