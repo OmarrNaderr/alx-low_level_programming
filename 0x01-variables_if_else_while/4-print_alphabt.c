d50e3a65ac6610c279235e368571e626b390143d
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-16 17:58:05 -0700

    Update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; )
+	{
+		if (alph != e && alph != q)
+		putchar(alph);
+	}
+		putchar('\n');
+
+	return (0);
+}
