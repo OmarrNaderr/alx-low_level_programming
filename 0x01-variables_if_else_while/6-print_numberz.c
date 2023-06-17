8fc97086414895d4ffd9f3c30f3fe0572ae7c1d6
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-16 18:27:53 -0700

    Update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-	char i = '0';
+	char i;
 
-	while( i <= '9' )
+	for (i = '0'; i <= '9'; i++)
 	{	putchar(i);
-		i++;
 	}
-		putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
