a9f5127301a07248c6487c1745fcc07e5ee50720
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-16 18:30:45 -0700

    Update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{	putchar(alph);
+	}
+		putchar('\n');
+
+	return (0);
+}
