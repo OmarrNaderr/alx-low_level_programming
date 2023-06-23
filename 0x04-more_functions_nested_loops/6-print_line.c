a832a12f2a8da121b383a0af9e8d34d72e0443dc
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-23 15:25:30 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+	int n;
+
+	for (i; i <= n ; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		_putchar('\n');
+		}
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	}
+}
