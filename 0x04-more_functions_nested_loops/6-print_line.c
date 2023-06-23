98d665571abbcbd46eff32162ca5896035b938f2
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-23 15:29:48 -0700

    Update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@ void print_line(int n)
 	int i;
 	int n;
 
-	for (i; i <= n ; i++)
+	for (i = 1; i <= n ; i++)
 	{
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
+		_putchar('_');
 		_putchar('\n');
-		}
+		
 		if (n <= 0)
+		{
 			_putchar('\n');
+		}
 	}
 }
