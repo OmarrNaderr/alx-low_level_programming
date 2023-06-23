71ee89efb0ec8741bc77ed0bcda9970304887542
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-23 14:29:13 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	char n;
+	if(n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(\n);
+
+	return(0);
+}
