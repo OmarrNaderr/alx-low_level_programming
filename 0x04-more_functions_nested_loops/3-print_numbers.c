52a31d66d43dd960ab4045ff21188a1c2801995a
OmarrNaderr <omar.nader.farouk@gmail.com>
2023-06-23 14:33:06 -0700

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
 void print_numbers(void)
 {
 	char n;
-	if(n = '0'; n <= '9'; n++)
+
+	for (n = '0'; n <= '9', n++)
 	{
 		_putchar(n);
 	}
-	_putchar(\n);
+	_putchar ("\n");
 
-	return(0);
+	return (0);
 }
